f496d5b99572e79b1766a9b6d15dff3c709f35ed
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:13:24 -0800

    Add files via upload

---
+++ pert2_latihan9.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form action="Koneksilatihan.php" method="post">
+Nama  : <input type="text" name="nama"><br>
+Jurusan : <input type="text"name="jurusan"><br>
+Nilai Tugas: <input type="text"name="tugas"><br>
+Nilai UTS : <input type="text"name="uts"><br>
+Nilai UAS : <input type="text"name="uas"><br>
+<input type="submit" value="Hitung">
+</form>
+</body>
+</html>
