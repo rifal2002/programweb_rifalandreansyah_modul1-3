cd5a08bf4fe27be09e736f105d1cc93dae265dff
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:05:50 -0800

    Add files via upload

---
+++ pert1_latihan5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pert1</title>
+</head>
+<body>
+    <?php
+    print"<center><font size=14 color='red'><b>BIODATA</b></font></center><br>";
+    print"<center><font size=12 color='blue'> Nama    :Rifal Andreansyah</font></center><br>";
+    print"<center><font size=12 color='blue'> Email   :Rifalandreansyah@gmail.com</center></font><br>";
+    print"<center><font size=12 color='blue'> Jurusan :Teknik Informatika</center></font><br>";
+    print"<center><font size=12 color='blue'> Hoby    :Renang</center></font><br>";
+    ?>
+</body>
+</html>
