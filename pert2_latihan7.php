cf58bc8a1457e3948fb9bb98ac81ae234a122b32
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:11:06 -0800

    Add files via upload

---
+++ pert2_latihan7.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form action="koneksi7.php" method="post">
+NamaBarang  : <input type="text" name="nmbrg"><br>
+HargaSatuan : <input type="text"name="hsbrg"><br>
+JumlahBarang: <input type="text"name="jmlbrg"><br>
+<input type="submit" value="kirim">
+<input type="reset" value="hapus">
+</form>
+</body>
+</html>
+
