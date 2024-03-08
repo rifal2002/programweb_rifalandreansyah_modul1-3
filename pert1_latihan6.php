a82a0c483cd8bff8ec22146e9edbaba634519647
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:06:04 -0800

    Add files via upload

---
+++ pert1_latihan6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <div>
+        <h1>Pesan Pertama Kali Belajar PHP</h1>
+        <p> 
+    <?php echo"<font color='red'>Sangat Menyenangkan Belajar PHP Native</font><br>";
+        echo"<font color='blue'>Harus Semangat Dan Rajin Menjadi Software Developer</font><br>";
+        
+        ?></p>
+    </div>
+</body>
+</html>
