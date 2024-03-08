edf861a5f70cb0a0908f8164891eb90fef6307f6
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:16:15 -0800

    Add files via upload

---
+++ pert3_latihan1.php
@@ -0,0 +1,23 @@
+<?php
+$ball ='purple';
+//Cobaubahke"yellow","blue","green","purple",atauwarnalain
+if ($ball=='red'){
+$redbox = $ball;
+echo "redbox:$redbox<br> \n";
+}elseif ($ball=='yellow'){
+$yellowbox=$ball;
+echo"yellowbox:$yellowbox<br>\n";
+}elseif ($ball=='blue'){
+$bluebox=$ball;
+echo "bluebox:$bluebox<br>\n";
+}elseif ($ball=='green'){
+$greenbox =$ball;
+echo "greenbox:$greenbox<br>\n";
+}elseif ($ball=='purple'){
+$purplebox=$ball;
+echo "purplebox:$purplebox<br>\n";
+}else{
+$colorlessbox=$ball;
+echo "colorlessbox:$colorlessbox<br>\n";
+}
+?>
