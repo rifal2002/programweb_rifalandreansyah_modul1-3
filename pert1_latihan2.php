85c9a2039f7186a572aa6ca49637ea12e84b2176
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:04:53 -0800

    Add files via upload

---
+++ pert1_latihan2.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Test Penyisipan PHP dan TML</title>
+</head>
+<body>
+<h2>1st Thing1st, Silakan identifikasikan diri Anda!</h2>
+<hrcolor="blue"size=4width=60%align=left>
+<?php 
+//BerikutiniadalahkodePHPyangdisisipkan
+Print"<b><font face= 'arial' size= 5 color= 'sienna' >Momentum tahun baru
+memang paling enak untuk di jadikan turning perbaikan diri <br>";
+print"Buthow? Begitu banyak kebiasaan buruk,begitu banyak masalah,begitu banyak tujuan,<br>";
+echo"begitu banyak mimpi,bla...bla...bla...Stop!tentukan prioritas...firstthing
+first!</font></b>";
+?>
+<h3>Selamat belajar PHP</h3>
+<?php
+print"<pre>";
+print"<center><font size= 14 color= 'magenta'>BIODATA </font> </center>";
+echo"<hr size= 4 width= 60% color='blue'>";
+print"<center><font size= 12 color= 'red'>    Nama     :EndarNirmala            </center></font><br>";
+print"<center><font size= 12 color= 'blue'>   Kelas    :6TPLPB                 </center></font><br>";
+print"<center><font size= 12 color= 'green'>  Jurusan  :Teknik Informatika    </center></font><br>";
+print"<center><font size= 12 color= 'purple'>  Hoby     :Traveling             </center></font><br></pre>";
+echo"<hr size= 4 width= 60% color='blue'>";
+?>
+</body>
+</html>
