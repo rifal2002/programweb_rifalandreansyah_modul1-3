b7ae7b8628d39cf089a89cb8cb11bdc0aa38ad0c
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:05:07 -0800

    Add files via upload

---
+++ pert1_latihan3.php
@@ -0,0 +1,9 @@
+<?php
+echo"<h1>Menampilkan Teks dalam PHP</h1>";
+echo("Hai apakabar semuanya<br>");
+echo"Belajar PHP tidak sulit namun perlu banyak berlatih<br>";
+echo"Bentuk lain","Mencetak dalam","PHP<br>";
+echo"Selamat Mencoba <br>";
+echo"PHP dapat Embed dalam HTML";
+echo"HTML dapat Embed dalam PHP";
+?>
