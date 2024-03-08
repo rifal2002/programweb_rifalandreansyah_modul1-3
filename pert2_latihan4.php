82c128033e8be61a6bfe342f2a1300088a5ebb9e
rifal2002 <133494390+rifal2002@users.noreply.github.com>
2024-03-08 09:10:08 -0800

    Add files via upload

---
+++ pert2_latihan4.php
@@ -0,0 +1,13 @@
+<?php
+$a1= 3;
+$b1= 4;
+$c1= $a1.$b1;
+//nilai$csekarang34,menggabungkanangka3dan4
+$d= $c1+1;
+echo "$d <br>";
+//nilai$d35,
+$e= "Number";
+$f= $e.$d;
+echo $f;
+//nilai$fmenjadiNumber13,akombinasi$edan$f
+?>
